f561db7714d62b1dd4badc1397ed116083e6ea91
unknown <shehanvindika429@gmail.com>
2026-09-08 00:13:12 +0530

    Development-AddFrontEndModelForProvince

---
+++ src/app/services/province.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProvinceService {
+
+  private readonly apiUrl = `${environment.apiBaseUrl}/Province`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllProvinces(): Observable<any> {
+    return this.http.get<any>(
+      `${this.apiUrl}/GetAll`
+    );
+  }
+}
