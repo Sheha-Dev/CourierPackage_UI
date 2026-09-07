2bc4ba48e127d318ac4b0beb82029604a0779d9f
unknown <shehanvindika429@gmail.com>
2026-09-08 00:17:14 +0530

    Development-CreateWarehouseMainView

--- src/app/pages/admin/warehouse/warehouse.ts
+++ src/app/pages/admin/warehouse/warehouse.ts
@@ -1,9 +1,60 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild
+} from '@angular/core';
+
+import {
+  CommonModule
+} from '@angular/common';
+import { WarehouseAdd } from './warehouse-add/warehouse-add';
+import { WarehouseList } from './warehouse-list/warehouse-list';
+import { WarehouseFullRequestDto } from '../../../models/warehouse';
+
 
 @Component({
-  imports: [],
   selector: 'app-warehouse',
-  styleUrl: './warehouse.scss',
+  standalone: true,
+  imports: [
+    CommonModule,
+    WarehouseAdd,
+    WarehouseList
+  ],
   templateUrl: './warehouse.html',
+  styleUrl: './warehouse.scss'
 })
-export class Warehouse {}
+export class Warehouse {
+
+  @ViewChild(WarehouseAdd)
+  warehouseAddComponent?: WarehouseAdd;
+
+  @ViewChild(WarehouseList)
+  warehouseListComponent?: WarehouseList;
+
+  selectedWarehouse:
+    WarehouseFullRequestDto | null = null;
+
+  editWarehouse(
+    warehouse: WarehouseFullRequestDto
+  ): void {
+
+    this.selectedWarehouse = warehouse;
+
+    setTimeout(() => {
+      this.warehouseAddComponent
+        ?.loadWarehouse(warehouse);
+
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    });
+  }
+
+  warehouseSaved(): void {
+
+    this.selectedWarehouse = null;
+
+    this.warehouseListComponent
+      ?.loadWarehouses();
+  }
+}
