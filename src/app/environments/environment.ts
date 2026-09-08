a9a1e9d047192fbb6d584c34c455676d9b39ad88
unknown <shehanvindika429@gmail.com>
2026-09-08 13:09:34 +0530

    AddNewApiURL

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   //apiBaseUrl: 'https://localhost:7236/api',
-  apiBaseUrl: 'https://currierpackagewebapi-haadexhheed0dxac.eastasia-01.azurewebsites.net/api',
+  apiBaseUrl: 'https://courierpackageapi-esbwhzdwdrb7dnh2.eastasia-01.azurewebsites.net/api',
 };
